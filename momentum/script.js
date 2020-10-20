4716c49ad90f8e8067335cc9397163acd33ac639
AleksTret <Aleks.Tret@gmail.com>
2020-10-20 17:24:45 +0300

    feat: make basic functionality

--- momentum/script.js
+++ momentum/script.js
@@ -1,113 +1,116 @@
-// DOM Elements
-const time = document.querySelector('.time'),
-  greeting = document.querySelector('.greeting'),
-  name = document.querySelector('.name'),
-  focus = document.querySelector('.focus');
+'use strict';
 
-// Options
-const showAmPm = true;
+const time = document.querySelector('.time');
+const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
+const goal = document.querySelector('.goal');
+const date = document.querySelector('.date');
+const editable = document.querySelectorAll('[date-editable]');
 
-// Show Time
-function showTime() {
-  let today = new Date(),
-    hour = today.getHours(),
-    min = today.getMinutes(),
-    sec = today.getSeconds();
+const monthsName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+                    'September', 'October', 'November', 'December'];
+const weekdaysName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  // Set AM or PM
-  const amPm = hour >= 12 ? 'PM' : 'AM';
-
-  // 12hr Format
-  hour = hour % 12 || 12;
-
-  // Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
-    sec
-  )} ${showAmPm ? amPm : ''}`;
-
-  setTimeout(showTime, 1000);
-}
-
-// Add Zeros
-function addZero(n) {
-  return (parseInt(n, 10) < 10 ? '0' : '') + n;
-}
-
-// Set Background and Greeting
-function setBgGreet() {
-  let today = new Date(),
-    hour = today.getHours();
-
-  if (hour < 12) {
-    // Morning
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-    greeting.textContent = 'Good Morning, ';
-  } else if (hour < 18) {
-    // Afternoon
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-    greeting.textContent = 'Good Afternoon, ';
-  } else {
-    // Evening
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/924T2Wv/night.jpg')";
-    greeting.textContent = 'Good Evening, ';
-    document.body.style.color = 'white';
-  }
-}
-
-// Get Name
-function getName() {
-  if (localStorage.getItem('name') === null) {
-    name.textContent = '[Enter Name]';
-  } else {
-    name.textContent = localStorage.getItem('name');
-  }
-}
-
-// Set Name
-function setName(e) {
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('name', e.target.innerText);
-      name.blur();
+class Momentum{
+    constructor(){
+        this.getName();
+        this.getGoal();
     }
-  } else {
-    localStorage.setItem('name', e.target.innerText);
-  }
-}
 
-// Get Focus
-function getFocus() {
-  if (localStorage.getItem('focus') === null) {
-    focus.textContent = '[Enter Focus]';
-  } else {
-    focus.textContent = localStorage.getItem('focus');
-  }
-}
-
-// Set Focus
-function setFocus(e) {
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('focus', e.target.innerText);
-      focus.blur();
+    redraw(){
+        this.redrawTime();
+        this.redrawBackground();
     }
-  } else {
-    localStorage.setItem('focus', e.target.innerText);
-  }
+
+    redrawTime() {
+        let today = new Date();
+    
+        time.innerHTML = `${today.getHours()}<span>:</span>${this.addZero(today.getMinutes())}<span>:</span>${this.addZero(today.getSeconds())}`;
+        date.innerHTML = `${weekdaysName[today.getDay()]}<span> </span>${today.getDate()}<span> </span>${monthsName[today.getMonth()]}`;
+
+        let that = this;
+        setTimeout(() => that.redrawTime(), 1000);
+    }
+
+    addZero(number){
+        return parseInt(number, 10) < 10 ? `0${number}` : number;
+    }
+
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }    
+
+    redrawBackground(){
+        let partDay = '';
+        switch (Math.floor((new Date()).getHours()/6)) {
+            case 0:
+                partDay = 'night';
+                break;
+            case 1:
+                partDay = 'morning';
+                break;
+            case 2:
+                partDay = 'day';
+                break;
+            case 3:
+            case 4:
+                partDay = 'evening';
+                break;
+            default:
+                partDay = '';
+                break;
+        }
+    
+        let imageNumber = this.addZero(this.getRandomInt(1, 20));
+    
+        document.body.style.backgroundImage = 
+            `url(assets/images/${partDay}/${imageNumber}.jpg)`;
+        greeting.textContent = `Good ${partDay}`;
+
+        let that = this;
+        setTimeout(() => that.redrawBackground(), 3600000);
+    }
+
+    getName(){
+        name.textContent = localStorage.getItem('name') || '[Enter your Name]';
+    }
+
+    getGoal(){
+        goal.textContent = localStorage.getItem('goal') || '[Enter your goal]';
+    }
+
+    onClick(event){
+        let className = event.currentTarget.classList.item(0).toString();
+        localStorage.setItem(className, event.currentTarget.innerText); 
+        event.currentTarget.innerText = '';   
+    }
+    
+    onKeydown(event){
+        if(event.code === 'Enter'){
+            let className = event.currentTarget.classList.item(0).toString();
+            localStorage.setItem(className, event.currentTarget.innerText); 
+            event.currentTarget.blur();
+        }
+    }
+    
+    whenBlur(event){
+        let className = event.currentTarget.classList.item(0).toString();
+        event.currentTarget.innerText = localStorage.getItem(className);
+    }
+    
+    setStorage(event){
+        let className = event.currentTarget.classList.item(0).toString();
+        localStorage.setItem(className, event.currentTarget.innerText);
+    }
 }
 
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
-focus.addEventListener('keypress', setFocus);
-focus.addEventListener('blur', setFocus);
+let momentum = new Momentum();
 
-// Run
-showTime();
-setBgGreet();
-getName();
-getFocus();
+editable.forEach(item => {
+    item.addEventListener('input', momentum.setStorage);
+    item.addEventListener('blur', momentum.whenBlur);
+    item.addEventListener('click', momentum.onClick);
+    item.addEventListener('keydown', momentum.onKeydown);
+})
+
+momentum.redraw();
