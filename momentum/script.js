f2d8ecfbac990f6d6f95f41c49b2bbc49a3eccb6
AleksTret <Aleks.Tret@gmail.com>
2020-10-21 15:57:29 +0300

    feat: make change background image

--- momentum/script.js
+++ momentum/script.js
@@ -6,6 +6,7 @@ const name = document.querySelector('.name');
 const goal = document.querySelector('.goal');
 const date = document.querySelector('.date');
 const editable = document.querySelectorAll('[date-editable]');
+const button = document.querySelector('.btn');
 
 const monthsName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
                     'September', 'October', 'November', 'December'];
@@ -15,6 +16,56 @@ class Momentum{
     constructor(){
         this.getName();
         this.getGoal();
+        this.setBackgroundList();
+        this.currentBackgroundIndex = 0;
+    }
+
+    setBackgroundList(){
+        this.backgroundImageList = [];
+
+        let filesName = []
+        for (let i = 0; i < 20; i++){
+            filesName[i] = `${this.addZero(i + 1)}.jpg`;
+        }
+
+        let daysPart = this.getDaysPart((new Date()).getHours());
+
+        for (let i = 0; i < 4; i++){
+            this.backgroundImageList = this.backgroundImageList.concat(this.getRandomArray(filesName).map(item => `assets/images/${daysPart}/${item}`));
+            daysPart = this.getNextDaysPart(daysPart);
+        }
+    }
+
+    getRandomArray(array){
+        return array
+            .map(function(elem,index) { return [elem, Math.random()]})
+            .sort(function(a,b){ return a[1] - b[1]})
+            .map(function(elem){return elem[0]});
+    }
+
+    getNextDaysPart(input){
+        let daysPart = ['morning', 'day', 'evening', 'night'];
+        let index = daysPart.indexOf(input);
+        index = index + 1 === daysPart.length ? 0 : ++index;
+        return daysPart[index];
+    }
+
+    getDaysPart(input){
+        let hour = parseInt(input, 10) || -1;
+        hour = 0 > hour || hour > 24 ? -1 : hour;
+        switch (Math.floor(hour/6)) {
+            case 0:
+                return 'night';
+            case 1:
+                return 'morning';
+            case 2:
+                return 'day';
+            case 3:
+            case 4:
+                return 'evening';
+            default:
+                return '';
+        }    
     }
 
     redraw(){
@@ -41,34 +92,22 @@ class Momentum{
     }    
 
     redrawBackground(){
-        let partDay = '';
-        switch (Math.floor((new Date()).getHours()/6)) {
-            case 0:
-                partDay = 'night';
-                break;
-            case 1:
-                partDay = 'morning';
-                break;
-            case 2:
-                partDay = 'day';
-                break;
-            case 3:
-            case 4:
-                partDay = 'evening';
-                break;
-            default:
-                partDay = '';
-                break;
-        }
+        let daysPart = this.getDaysPart((new Date()).getHours());
     
         let imageNumber = this.addZero(this.getRandomInt(1, 20));
-    
+
         document.body.style.backgroundImage = 
-            `url(assets/images/${partDay}/${imageNumber}.jpg)`;
-        greeting.textContent = `Good ${partDay}`;
+            `url(assets/images/${daysPart}/${imageNumber}.jpg)`;
+        greeting.textContent = `Good ${daysPart}`;
 
         let that = this;
         setTimeout(() => that.redrawBackground(), 3600000);
+    }
+
+    changeBackground(source){
+        let background = document.createElement('img');
+        background.src = source;
+        background.onload = () => {document.body.style.backgroundImage = `url(${source})`; };
     }
 
     getName(){
@@ -102,6 +141,14 @@ class Momentum{
         let className = event.currentTarget.classList.item(0).toString();
         localStorage.setItem(className, event.currentTarget.innerText);
     }
+
+    nextBackground(){
+        if (this.currentBackgroundIndex >= this.backgroundImageList.length){
+             this.currentBackgroundIndex = 0;
+        }
+        this.changeBackground(this.backgroundImageList[this.currentBackgroundIndex]);
+        this.currentBackgroundIndex++
+    }
 }
 
 let momentum = new Momentum();
@@ -112,5 +159,7 @@ editable.forEach(item => {
     item.addEventListener('click', momentum.onClick);
     item.addEventListener('keydown', momentum.onKeydown);
 })
+
+button.addEventListener('click', () => momentum.nextBackground());
 
 momentum.redraw();
