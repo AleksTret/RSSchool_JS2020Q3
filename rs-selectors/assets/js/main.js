10a540034fc1244cc4c4f29530013cecbc4c8001
AleksTret <Aleks.Tret@gmail.com>
2020-11-26 16:23:06 +0300

    feat: add menu, create game from json, add fonts

--- rs-selectors/assets/js/main.js
+++ rs-selectors/assets/js/main.js
@@ -1,39 +1,109 @@
-var levels = [
-    {
-      helpTitle : "Select elements by their type",
-      selectorName : "Type Selector",
-      doThis : "Select the plates",
-      selector : "plate",
-      syntax : "A",
-      help : "Selects all elements of type <strong>A</strong>. Type refers to the type of tag, so <tag>div</tag>, <tag>p</tag> and <tag>ul</tag> are all different element types.",
-      examples : [
-        '<strong>div</strong> selects all <tag>div</tag> elements.',
-        '<strong>p</strong> selects all <tag>p</tag> elements.',
-      ],
-      boardMarkup: `
-      <plate/>
-      <plate/>
-      `
-    }
-]
-
-const plate = {
-    init(){
-        const plate =  document.createElement("div");
-        plate.classList.add("plate");
-
-        return plate;
-    },
-}
+'use strict';
 
 const game = {
-    init(){
-        const table = document.getElementById("table");
+    async init(urlGameLevels){
+        this._initPageElement();
+        this.levels = await this._loadLevels(urlGameLevels);
+        this._createLevel(this.levels.get("2"));
+    },
 
-        table.appendChild(plate.init());
-        table.appendChild(plate.init());
+    async _loadLevels(urlGameLevels){   
+        const json = await this._loadJSON(urlGameLevels);
+        return this._jsonToMap(json);
+    },
+
+    async _loadJSON(url){
+        const response = await fetch(url);
+        return await response.json();
+    },
+
+    _jsonToMap(json){
+        let result = new Map();
+    
+        Object.entries(json).forEach((item) => {
+            result.set(item[0], item[1]);
+        })
+        return result;
+    },
+
+    _initPageElement(){
+        this._getMenuElement();
+        this._getCodeAreaElement();
+        this._getTableElement();
+    },
+
+    _getMenuElement(){
+        this.task = document.getElementById("task");
+        this.taskLevel = document.getElementById("taskLevel");
+        this.taskName = document.getElementById("taskName");
+        this.taskDescription = document.getElementById("taskDescription");
+        this.taskSyntax = document.getElementById("taskSyntax");
+        this.taskHint = document.getElementById("taskHint");
+        this.taskExamples = document.getElementById("taskExamples");
+    },
+
+    _getTableElement(){
+        this.table = document.getElementById("table");
+    },
+
+    _getCodeAreaElement(){
+        this.codeArea = document.getElementById("codeArea");
+    },
+
+    _createLevel(task){
+        // fill menu element
+        this.taskLevel.innerHTML = `Level ${task.level} of 15`;
+        this.taskName.innerHTML = task.name;
+        this.taskDescription.innerHTML = task.description;
+        this.taskSyntax.innerHTML = task.syntax;
+        this.taskHint.innerHTML = task.hint;
+        this.taskExamples.appendChild(this._createExamples(task.examples));
+
+        // fill code area
+        this.codeArea.innerHTML = this._createCodeArea(task.markup);
+
+        // create elements on the table
+        this.table.appendChild(this._createElementOnTable(task.markup));
+    },
+
+    _createPlate(plate){
+        const result =  document.createElement("div");
+        result.classList.add("plate");
+        plate.includes("fancy") && result.classList.add("fancy");
+        return result;
+    },
+
+    _createElementOnTable(markup){
+        const result = document.createDocumentFragment();
+        markup.forEach(item => {
+            if(item.includes("plate")){
+                result.appendChild(this._createPlate(item));
+            }; 
+        });
+        return result;
+    },
+
+    _createCodeArea(markup){
+        let result = '&lt;div class="table"&gt;<br>';
+        markup.forEach(item => {
+            result += `&nbsp;&nbsp;&nbsp;&lt;${item}/&gt;<br>`;
+        });
+        result += '&lt;/div&gt;';
+        return result;
+    },
+
+    _createExamples(examples){
+        const result = document.createDocumentFragment();
+        examples.forEach(item => {
+            const element = document.createElement("div");
+            element.classList.add("example");
+            element.innerHTML = item;
+            result.appendChild(element) 
+        });
+        return result;
     }
 }
 
+const urlGameLevels = "assets/json/levels.json";
 
-window.addEventListener("DOMContentLoaded",  () => game.init());
+window.addEventListener("DOMContentLoaded",  () => game.init(urlGameLevels));
